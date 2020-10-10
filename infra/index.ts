eeb8a0550416003cf9e8ba039eb3e8ec8f68ecf1
Joa Riski <riski.joa@gmail.com>
2020-10-11 01:16:21 +0300

    Add kiertotalo.us

--- infra/index.ts
+++ infra/index.ts
@@ -2,10 +2,19 @@ import { deployContainer } from "./apps";
 import { provider } from "./clustersetup";
 
 
+// deployContainer("mailhog", {
+//     domain: "mailhog.aisodoi.com",
+//     image: "mailhog/mailhog",
+//     port: 8025,
+//     release: "test",
+//     replicas: 1,
+// }, provider);
+
+
 deployContainer("mailhog", {
-    domain: "mailhog.aisodoi.com",
-    image: "mailhog/mailhog",
-    port: 8025,
-    release: "test",
+    domain: "kiertotalo.us",
+    image: "fransle/kiertotalous",
+    port: 80,
+    release: "1",
     replicas: 1,
 }, provider);
